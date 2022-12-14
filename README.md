49b1817b9cab248cf863ca9830b96c127fbb9f9f
Stefano Furin <furins@gmail.com>
2022-12-14 14:52:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# YAMLconfig
+
+A very simple library to read a YAML configuration file and return a dictionary
+
+## INSTALL
+
+    python -m pip install https://github.com/furins/yamlconfig/tarball/main
+
+## AUTHOR
+
+Stefano Furin
+
+## LICENSE
+
+MIT
---
+++ src/yamlconfig/loader.py
@@ -0,0 +1,25 @@
+"""libreria per la gestione di file di configurazione YAML"""
+from pathlib import Path
+
+import yaml
+
+class ImproperConfiguration(Exception):
+    """Eccezione sollevata quando si riscontra un file non valido e non recuperabile
+    """
+
+def loadconfig():
+    """
+    Carica il file di configurazione.
+
+    In caso di problemi termina il programma.
+
+    """
+    configpath = Path("./config.yaml")
+    if configpath.is_file():
+        with open(configpath, "r", encoding="utf-8") as configfile:
+            try:
+                return yaml.safe_load(configfile)
+            except yaml.YAMLError as exc:
+                raise ImproperConfiguration("file di configurazione non valido") from exc
+    else:
+        raise FileNotFoundError(f"file di configurazione non trovato: {configpath.resolve()}")
